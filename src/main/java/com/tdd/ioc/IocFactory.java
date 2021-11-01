90d0ad127b7face74bfdff39aee363370292561c
huangweizhe <huangweizhe@meituan.com>
2021-11-01 16:08:23 +0800

    测试通过

--- src/main/java/com/tdd/ioc/IocFactory.java
+++ src/main/java/com/tdd/ioc/IocFactory.java
@@ -6,22 +6,38 @@ import java.util.Map;
 public class IocFactory {
 
     private final Map<Class<?>, Object> map = new HashMap<>();
+    private final Map<Class<?>, Scope> scopeMap = new HashMap<>();
 
     public <T> void bind(Class<T> aClass) {
-        try {
-            T newInstance = aClass.newInstance();
-            map.put(aClass, newInstance);
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
+        bind(aClass, Scope.SINGLETON);
+    }
+
+    public <T> void bind(Class<T> aClass, Scope scope) {
+        if (scope == Scope.SINGLETON) {
+            try {
+                T newInstance = aClass.newInstance();
+                map.put(aClass, newInstance);
+                scopeMap.put(aClass, Scope.SINGLETON);
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException();
+            }
+        } else {
+            scopeMap.put(aClass, Scope.PROTOTYPE);
         }
     }
 
     public <T> T getInstance(Class<T> aClass) {
-        T instance = (T) map.get(aClass);
-        if (instance == null) {
+        Scope scope = scopeMap.get(aClass);
+        if (scope == null) {
             throw new RuntimeException();
-        } else {
+        } else if (scope == Scope.SINGLETON) {
             return (T) map.get(aClass);
+        } else {
+            try {
+                return aClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException();
+            }
         }
     }
 }
---
+++ src/main/java/com/tdd/ioc/Scope.java
@@ -0,0 +1,7 @@
+package com.tdd.ioc;
+
+public enum Scope {
+    SINGLETON,
+    PROTOTYPE
+
+}
