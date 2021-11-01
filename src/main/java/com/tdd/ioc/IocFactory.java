57e2a4485a307232ddd366168eb70e3a3f78a64b
huangweizhe <huangweizhe@meituan.com>
2021-11-01 15:21:34 +0800

    第一次编译通过但是运行不通过

---
+++ src/main/java/com/tdd/ioc/IocFactory.java
@@ -0,0 +1,11 @@
+package com.tdd.ioc;
+
+public class IocFactory {
+    public <T> void bind(Class<T> aClass) {
+
+    }
+
+    public <T> T getInstance(Class<T> aClass) {
+        return null;
+    }
+}
